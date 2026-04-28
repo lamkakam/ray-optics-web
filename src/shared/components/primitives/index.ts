c05696e63d4cb824a1f045cbda9af547efbd8037
lamkakam <6467358+lamkakam@users.noreply.github.com>
2026-04-28 19:00:37 +0100

    (human) remove a component that is no longer in use

--- src/shared/components/primitives/index.ts
+++ src/shared/components/primitives/index.ts
@@ -13,7 +13,6 @@ export * from "./NavLink";
 export * from "./Paragraph";
 export * from "./RadioInput";
 export * from "./Select";
-export * from "./SetButton";
 export * from "./Switch";
 export * from "./Table";
 export * from "./Tabs";
