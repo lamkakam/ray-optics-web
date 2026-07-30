6f7becd7ddf32c4b7d569caf7511cfeedf9a87e7
lamkakam <6467358+lamkakam@users.noreply.github.com>
2026-07-30 02:57:05 +0100

    (human) remove a useless test

--- __tests__/jestConfig.test.ts
+++
@@ -1,7 +0,0 @@
-import jestConfig from "../jest.config";
-
-describe("jestConfig", () => {
-  it("excludes generated static exports from canonical test discovery", () => {
-    expect(jestConfig.testPathIgnorePatterns).toContain("<rootDir>/out/");
-  });
-});
